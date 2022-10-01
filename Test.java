070ee4d60067722ea37afc5842cb6ae2c0554b48
udhayadharani <udhayarajendran7090@gmail.com>
2022-10-01 13:16:21 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Swift a1=new Swift("Swift","VXI",165);
+        System.out.println(a1.name);
+        System.out.println(a1.model);
+        System.out.println(a1.speed);
+        }
+    }
+
